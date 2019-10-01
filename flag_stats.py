22bb0be9f13ac305a522ca2de6175114362968c3
Stav Grossfeld <stav.grossfeld@gmail.com>
2019-10-01 16:12:33 -0700

    add code

--- flag_stats.py
+++ flag_stats.py
@@ -1,18 +1,35 @@
 import os
-for decimal in [.01, .1, .5, .9, .99]:
-    print(decimal)
-    file_var = "%d_sample.bam" % decimal
-
-    # extract sample
-    sample_cmd = "samtools view ../SIGAF1_144520_mRNA.bam -s $arg_var -b -@12 > %s" % file_var
 
 
-for decimal in [.01, .1, .5, .9, .99]:
-    print(decimal)
-    file_var = "%d_sample.bam" % decimal
-    sample_cmd = "samtools view ../SIGAF1_144520_mRNA.bam -s $arg_var -b -@12 > %s" % file_var
+def create_sample_files(bam_file_name):
+    print("running sample retreival: \n\n")
+    os.mkdir("trial")
+    for decimal in [.01, .1, .5, .9, .99]:
+        print("%s: complete") % decimal
+        file_var = "%d_sample.bam" % decimal
 
-    cmd_1 = "printf '. % s \t' >> log.txt" % decimal
-    cmd_2 = "samtools flagstat %s - @12 | awk 'NR==4{print $1}' | cat | xargs > file1.tmp" % file_var
-    cmd_2 = "samtools flagstat %s - @12 | awk 'NR==5{print $1}' | cat | xargs > file2.tmp" % file_var
-    cmd_3 = "paste file1.tmp file2.tmp >> log.txt"
+        # extract sample
+        sample_cmd = "samtools view ../%s -s $arg_var -b -@12 > %s" % bam_file_name, file_var
+
+        os.system(sample_cmd)
+
+
+def create_flag_stats():
+    print("running flag stats: \n\n")
+    for decimal in [.01, .1, .5, .9, .99]:
+        print("%s: complete") % decimal
+        file_var = "%d_sample.bam" % decimal
+
+        cmd_1 = "printf '. % s \t' >> log.txt" % decimal
+        cmd_2 = "samtools flagstat %s - @12 | awk 'NR==4{print $1}' | cat | xargs > file1.tmp" % file_var
+        cmd_3 = "samtools flagstat %s - @12 | awk 'NR==5{print $1}' | cat | xargs > file2.tmp" % file_var
+        cmd_4 = "paste file1.tmp file2.tmp >> log.txt"
+
+        os.system(cmd_1)
+        os.system(cmd_2)
+        os.system(cmd_3)
+        os.system(cmd_4)
+
+
+create_sample_files("SIGAF1_144520_mRNA.bam")
+create_flag_stats()
