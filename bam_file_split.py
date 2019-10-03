bffe3db6c5787f2aeb1afa2364ad0950d7baef87
Stav Grossfeld <stav.grossfeld@gmail.com>
2019-10-03 12:33:39 -0700

    fix script

--- bam_file_split.py
+++ bam_file_split.py
@@ -9,9 +9,8 @@ file_name = "99_sample.bam"
 create_urz_cmd = "samtools view %s | cut -f19-25 >> urz_%s.txt" % (
     file_name, file_name.replace(".bam", ""))
 
+os.system(create_urz_cmd)
 urz_file_name = "urz_%s.txt" % (file_name.replace(".bam", ""))
-
-# os.system(create_urz_cmd)
 
 df = pd.read_csv(urz_file_name, delimiter="\t")
 df.columns = ["barcode"]
