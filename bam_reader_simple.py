932243b736927b40b95d49c9516dcb8398252203
Stav Grossfeld <stav.grossfeld@gmail.com>
2019-10-04 13:01:45 -0700

    push

--- bam_reader_simple.py
+++ bam_reader_simple.py
@@ -1,13 +1,23 @@
 import sys
+import os
 import re
 from tqdm import tqdm
+
+import mmap
+
+
+def get_num_lines():
+    os.system("samtools view ~/Desktop/bam_split/01_sample.bam | wc -l")
+    for line in sys.stdin:
+        print(line)
 
 
 def main():
 
     umi_ct_dict = {}
     rd_ct = 0
-    for line in tqdm(sys.stdin):
+
+    for line in tqdm(sys.stdin, total=2624306):
         # print(line)
         umi = re.findall(r"UR:Z:\w*", line)
 
@@ -31,10 +41,12 @@ def main():
 
     umi_dict = {"deduped_reads": deduped_reads, "rd_ct": rd_ct}
 
-    sequence_saturation = 1 - float(deduped_reads / rd_ct)
+    sequence_saturation = 1 - \
+        float(umi_dict["deduped_reads"] / float(umi_dict["rd_ct"]))
     print("\n metrics: ", umi_dict)
     print("\n sequence_saturation: ", sequence_saturation)
 
 
 if __name__ == "__main__":
-    main()
+    get_num_lines()
+    # main()
--- fraction_bam.py
+++ fraction_bam.py
@@ -19,18 +19,19 @@ def create_sample_files(bam_file_name):
 
     sample_folder = "./%s_sample_stats/" % bam_file_name.replace(
         "_mRNA.bam", "")
+
     if not os.path.exists(sample_folder):
         os.mkdir(sample_folder)
         os.chdir(sample_folder)
     else:
         os.chdir(sample_folder)
     for decimal in [.01, .1, .5, .9, .99]:
-        print("%s: complete" % decimal)
+        print("%s: started" % decimal)
         file_var = "%s_sample.bam" % str(decimal).replace("0.", "")
 
         # extract sample
         print(bam_file_name, file_var)
-        sample_cmd = "samtools view ../%s -s %s -@12 > %s" % (
+        sample_cmd = "samtools view ../%s -s %s -b -@12 > %s" % (
             bam_file_name, decimal, file_var)
         print(sample_cmd)
         os.system(sample_cmd)
