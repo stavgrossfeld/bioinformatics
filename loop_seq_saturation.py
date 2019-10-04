8756e1a9c0731ce8aae99eadf09c6201ec95289b
Stav Grossfeld <stav.grossfeld@gmail.com>
2019-10-04 14:26:21 -0700

    fix

--- loop_seq_saturation.py
+++ loop_seq_saturation.py
@@ -4,23 +4,26 @@ import sys
 from multiprocessing import Pool
 
 
-def main(folder):
+def main(folder, multi):
 
     os.chdir(folder)
     bam_files = glob.glob('./*.bam')
 
     number_of_workers = 4
-    with Pool(number_of_workers) as p:
-        # Do something with pool here
-        for filename in bam_files:
+    # if multi == True:
+    # with Pool(number_of_workers) as p:
+    # Do something with pool here
 
-            os.system("touch ./log.txt")
-            cmd = "samtools view %s | python ~/bioinformatics_scripts/umi_dict.py %s " % (
-                filename, filename)
+    for filename in bam_files:
 
-            os.system(cmd)
+        os.system("touch ./log.txt")
+        cmd = "samtools view %s | python ~/bioinformatics_scripts/umi_dict.py %s " % (
+            filename, filename)
+
+        os.system(cmd)
 
 
 if __name__ == "__main__":
     folder = sys.argv[1]
-    main(folder)
+    multi = sys.argv[2]
+    main(folder, multi)
