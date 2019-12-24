7755e573d52d7f383793959d32428b0ade23c482
Stav Grossfeld <stav.grossfeld@gmail.com>
2019-12-23 16:20:49 -0800

    mongo script

--- index_hopping/filter_bams/filter_bam_mongo.py
+++ index_hopping/filter_bams/filter_bam_mongo.py
@@ -1,4 +1,3 @@
-import sqlite3
 """Script to filter index hopped or seen once in bams
 cb_umi but different read name = index hopping
 """
@@ -12,58 +11,37 @@ import subprocess
 import numpy as np
 import mmap
 import pandas as pd
+from pymongo import MongoClient
 pd.set_option('display.width', 1000)
 pd.options.display.max_colwidth = 100
 
 
-def main_sql():
-    db_file = "./bam.db"
+def main_mongo():
 
-    create_table_sql = """ DROP TABLE IF EXISTS tmp_bam_reads; CREATE TABLE IF NOT EXISTS tmp_bam_reads (
-                                        read text text,
-                                        bam_line text
-                                    ); """
+    myclient = MongoClient("mongodb://localhost:27017/")
 
-    # create a database connection
-    conn = sqlite3.connect(db_file)
-   # create tables
-    if conn is not None:
-        # create projects table
-
-        c = conn.cursor()
-        c.executescript(create_table_sql)
-        conn.commit()
-        print("created sql reads table")
-        return conn
-    else:
-        print("Error! cannot create the database connection.")
+    mydb = myclient["bam_db"]
+    mycol = mydb["bam_file"]
+    mycol.drop()
+    return mycol
+# create tables
 
 
-def insert_reads_sql(conn, data):
-    # sql_query = """INSERT INTO tmp_bam_reads VALUES("blahblahblah","yallahyallahyallah")"""
+def insert_reads_mongo(mycol, data):
+    # mongo_query = """INSERT INTO tmp_bam_reads VALUES("blahblahblah","yallahyallahyallah")"""
 
-    c = conn.cursor()
-    c.executemany("""
-    INSERT INTO
-        tmp_bam_reads
-        (read, bam_line)
-    VALUES
-        (:read, :bam_line)""", data)
-
-    conn.commit()
+    x = mycol.insert_many(data)
+    # print(x.inserted_ids)
 
 
-def query_read_sql(conn, read_name):
+def query_read_mongo(mycol, read_name):
 
-    c = conn.cursor()
-    qry = """SELECT bam_line FROM tmp_bam_reads WHERE read = "%s"; """ % read_name
-    c.execute(qry)
-    bam_line = c.fetchone()[0]
-
+    bam_line = mycol.find({"read_name": read_name}, {"line": 1})
+    bam_line = bam_line[0]['line']
     return bam_line
 
 
-def main(number_of_lines, conn):
+def main(number_of_lines, mycol):
     """ two dictionaries:
     cb_umi: each key is a cb_umi and a list of its read names
     multi_map_index_hop_dict: each key is a cb_umi and contains its index_hopped_reads and multi_mapped_reads
@@ -85,29 +63,25 @@ def main(number_of_lines, conn):
     pcr_replicate_bam = open("pcr_replicate.bam", "a")
 
     for ix, line in enumerate(tqdm(sys.stdin, total=number_of_lines)):
-
-        # for ix, line in enumerate(read_bam_sample.readlines()):
-        # print(line)
-       # print(ix)
+        ix = ix + 1
         if ix % 100000 == 0:
-            print(ix)
-            insert_reads_sql(conn, cb_umi_line_dict)
+
+            insert_reads_mongo(mycol, cb_umi_line_dict)
+
             cb_umi_line_dict = []
+            continue
 
         bam_line = line.split()
-
         read_name = bam_line[0]
         is_pcr_replicate = int(
             bam_line[1]) / 1024 or int(bam_line[1]) / 1040 == 1  # samtools pcr flag
 
         cb = re.findall(r"CB:Z:\w*", line)
         umi = re.findall(r"UR:Z:\w*", line)
-
         if len(cb) == 0:
-            print(cb)
             continue
-
         umi = umi[0].strip()
+
         cb = cb[0].strip()
 
         cb_umi = "".join([cb, "_", umi])  # , "_", chr_location_mega])
@@ -115,7 +89,7 @@ def main(number_of_lines, conn):
         if cb_umi not in cb_umi_read_dict:
             # if read name not in cb_umi dict, create  a list
             cb_umi_read_dict[cb_umi] = {read_name: 1}
-            cb_umi_line_dict.append((read_name, line))
+            cb_umi_line_dict.append({"read_name": read_name, "line": line})
 
         else:
 
@@ -139,16 +113,17 @@ def main(number_of_lines, conn):
 
             # once the read has gone through the flow append it to the original cb_umi_dict
 
+    # insert last one
+    insert_reads_mongo(mycol, cb_umi_line_dict)
     print("\n creating seen once bam: ")
     # print(cb_umi_line_dict)
     for cb_umi in tqdm(cb_umi_read_dict, total=len(cb_umi_read_dict)):
         if len(cb_umi_read_dict[cb_umi]) == 1:
             #  print(cb_umi_line_dict[cb_umi][line])
             read_name = list(cb_umi_read_dict[cb_umi].items())[0][0]
-            returned_bam_line = query_read_sql(conn, read_name)
+            returned_bam_line = query_read_mongo(mycol, read_name)
             seen_once_bam.write(returned_bam_line)
 
-    conn.close()
     seen_once_bam.close()
     index_hop_bam.close()
     multi_map_bam.close()
@@ -159,6 +134,5 @@ def main(number_of_lines, conn):
 
 if __name__ == "__main__":
     number_of_lines = sys.argv[1]
-    #number_of_lines = 1000
-    conn = main_sql()
-    main(number_of_lines, conn)
+    mycol = main_mongo()
+    main(number_of_lines, mycol)
