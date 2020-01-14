094c375b201f17395140effed04c2f8038e1f1ca
Stav Grossfeld <stav.grossfeld@gmail.com>
2020-01-14 12:22:59 -0800

    add activate filter bam

---
+++ index_hopping/filter_bams/filter_bam_activate.py
@@ -0,0 +1,55 @@
+"""script to filter bam files
+ex run: python ~/bioinformatics_scripts/loop_seq_saturation.py ./ True
+"""
+
+import glob
+import os
+import sys
+from multiprocessing import Pool
+
+
+def main(bam_file):
+
+    # rint(multi)
+    cwd = os.getcwd()
+
+    read_types_dir = "%s/%s_read_types" % (cwd, bam_file)
+
+    if not os.path.isdir(read_types_dir):
+        os.mkdir(read_types_dir)
+    os.chdir(read_types_dir)
+
+    file_types = ["index_hop.bam", "seen_once.bam",
+                  "multi_map.bam", "pcr_replicate.bam"]
+
+    # p = Pool(4)
+    # p.map(create_files, file_type, bam_file)
+
+    for file_name in file_types:
+        create_files(file_name, bam_file)
+
+    call_cmd(bam_file)
+
+
+def create_files(file_type, bam_file):
+    print(bam_file)
+    file_type = file_type
+
+    cmd = "samtools view -H ../%s > ./%s" % (bam_file, file_type)
+   # print(cmd)
+    os.system(cmd)
+
+
+def call_cmd(bam_file):
+
+    cmd = "samtools view ../%s | head -10000 | python ~/bioinformatics_scripts/index_hopping/filter_bams/filter_bam_mongo.py %s " % (
+        bam_file, 10000)
+
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    os.chdir("/Users/stav/Desktop")
+    os.chdir(os.path.abspath("bam_split"))
+    bam_file = "04_sample.bam"  # argv[1]
+    main(bam_file)
