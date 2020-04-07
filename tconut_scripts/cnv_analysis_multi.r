0ba1d340d8bb4e647ef0770c8c9bb6d7996033cb
Stav Grossfeld <stav.grossfeld@gmail.com>
2020-04-06 17:56:31 -0700

    update cnv_analysis

--- tconut_scripts/cnv_analysis_multi.r
+++ tconut_scripts/cnv_analysis_multi.r
@@ -5,7 +5,7 @@ files <-list.files(path="./tconut_cna_files", pattern="*.tsv", full.names=TRUE, 
 
 #SZ
 files_to_process <- c("3993", "4213", "4301", "4385", "4404", "4413", "4463", "4469", "4812", "4114", "4656", "3985", "4142", "4284", "4357", "4508", "4984", "4100", "4564", "4619", "4646", "4661", "4735", "4938", "4429", "4296", "4801")
-
+#files_to_process <- c("3993", "4213")
 #grepl(files_to_process)
 #grepl(files_to_process[[1]],files)
 
@@ -19,7 +19,7 @@ for (single_file in files_to_process) {
     
   for (file_name in files_like) {
       df <- read.table(file_name, header = T)
-      file_name<-gsub(".cna.tsv", x=gsub("./tconut_cna_files",x=file_name,""),"")
+      file_name<-gsub(".cna.tsv", x=gsub("./tconut_cna_files/",x=file_name,""),"")
       file_name_list <- append(file_name, file_name_list)
       df_list <- append(list(df), df_list)
     }
@@ -91,14 +91,38 @@ no_cores <- 12
 
 cl <- makeCluster(no_cores)
 
-create_cnv_calls <- function(i, df_list) {
+create_cnv_calls <- function(i, df_list, min_cnv_length) {
   df <- df_list[[i]]
   df$name <- i
   df <- create_amp_del(df)
-  df <- find_cnvs(df, min_cnv_length = 1e6)
-  return(df)
+  df <- find_cnvs(df, min_cnv_length)
+  found_cnvs <- df[df$cnv_start_end==2,]
+  found_cnvs$Position_start <- found_cnvs$Position - found_cnvs$cnv_length
+  #print(found_cnvs)
+  return(found_cnvs)
 }
-df_new_list <- lapply(X=names(df_list), FUN=create_cnv_calls, df_list)
+
+run_cnv_and_write_file <- function(df_list, min_cnv_length) {
+  df_found_cnv_list <- lapply(X=names(df_list), FUN=create_cnv_calls, df_list, min_cnv_length)
+  #return(df_found_cnv_list)
+  found_cnvs <- do.call("rbind", df_found_cnv_list)
+
+
+  found_bed <- found_cnvs[,c("Chr", "Position_start", "Position","name", "amp_del")]
+  write.table(found_bed, file = paste0("./found_cnvs/multi_found_cnvs_",min_cnv_length,".bed"), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+  found_seg <- found_cnvs[,c("name","Chr", "Position_start", "Position","cnv_length","Fold.Change")]
+  write.table(found_seg, file = paste0("./found_cnvs/multi_found_cnvs_",min_cnv_length,".seg"), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
+}
+run_cnv_and_write_file(df_list, 1e5) #100k
+
+
+run_cnv_and_write_file(df_list, 5e5) #500k
+run_cnv_and_write_file(df_list, 1e6) # 1mil
+
+
+
+
 
 
 stopCluster(cl)
@@ -106,18 +130,7 @@ stopCluster(cl)
 
 #rbind(df_list)
 
-df_final <- do.call("rbind", df_new_list)
-found_cnvs <- df_final[df_final$cnv_start_end==2,]
 
-found_cnvs$Position_start <- found_cnvs$Position - found_cnvs$cnv_length
-
-found_bed <- found_cnvs[,c("Chr", "Position_start", "Position","name", "amp_del")]
-
-write.table(found_bed, file = "./found_cnvs/multi_found_cnvs.bed", quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
-
-found_seg <- found_cnvs[,c("name","Chr", "Position_start", "Position","cnv_length","Fold.Change")]
-
-write.table(found_seg, file = "./found_cnvs/multi_found_cnvs.seg", quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
 
 
      
