8214017f7c406d0373b8fe6640f81e56282645a6
Stav Grossfeld <stav.grossfeld@gmail.com>
2020-05-04 11:50:05 -0700

    run for faulty samples

--- tconut_scripts/tconut_pipeline.sh
+++ tconut_scripts/tconut_pipeline.sh
@@ -1,7 +1,8 @@
 CTRL_BAM=$1
 GENDER_FLAG=$2
 BAMFILES_DIR=$PWD
-for BAMFILE in `ls *.bam`
+#for BAMFILE in `ls *.bam`
+for BAMFILE in `cat ../samples_to_run.txt`
 	do
 		if [ "$BAMFILE" != "$CTRL_BAM" ];
 	then sbatch /home/rcf-40/grossfel/tconut_scripts/tconut_pipeline.slurm $BAMFILE $CTRL_BAM $BAMFILES_DIR $GENDER_FLAG;
